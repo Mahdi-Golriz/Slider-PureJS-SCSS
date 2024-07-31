dcc0c81f7434fe969897e8e108fb004777d0c91f
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2024-07-31 15:37:44 +0200

    final

--- slider.js
+++ slider.js
@@ -1,117 +1,112 @@
-let Slider = function ({
-  data,
-  firstActiveIndex,
-  intervalTime,
-  width,
-  height,
-}) {
+let Slider = function ({ firstActiveIndex = 0, intervalTime, width, height }) {
   let _config = {
-    data,
     activeIndex: firstActiveIndex,
     autoSlideInterval: intervalTime,
     containerWidth: width,
     containerHeight: height,
-    numSlides: data.length,
+    slides: Array.from(document.querySelector("ul.slides").children),
   };
 
   let intervalId;
 
-  console.log(document.querySelector("ul.container"));
-
-  function _init() {
-    const slides = _config.data.map((item) => {
-      return `<li class='slide'>
-                  <figure>
-                    <figcaption>
-                        ${item.title}
-                <p>${item.description}</p>
-                    </figcaption>
-                    <img src=${item.url}  alt=${item.alt} />
-              
-                  </figure>
-                </li>
-              `;
-    });
-
-    const dots = _config.data.map(() => {
-      return `<li>
-                      <a class='dot'></a>
-                  </li>
-              `;
-    });
-
-    document.querySelector("ul.container").innerHTML = slides.join("");
-    document.querySelector("ul.dots").innerHTML = dots.join("");
+  function init() {
+    _generateDots();
+    _updateDots();
+    _addEventListeners();
+    _autoPlay();
+    _dynamicDimensions();
   }
 
-  function _moveTo() {
-    const SlidesArray = Array.from(
-      document.querySelector("ul.container").children
-    );
+  function _generateDots() {
+    const dotsContainer = document.querySelector("ul.dots");
+    dotsContainer.innerHTML = "";
 
-    SlidesArray.forEach((slide, i) => {
-      slide.classList.toggle("active", i === _config.activeIndex);
-      if (_config.activeIndex === _config.numSlides - 1) {
-        slide.classList.toggle("next", i === 0);
-      } else slide.classList.toggle("next", i === _config.activeIndex + 1);
+    _config.slides.forEach((_, i) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.href = "#";
+      a.addEventListener("click", (e) => {
+        e.preventDefault();
+        _moveTo(i);
+      });
 
-      if (_config.activeIndex === 0) {
-        slide.classList.toggle("prev", i === _config.numSlides - 1);
-      } else slide.classList.toggle("prev", i === _config.activeIndex - 1);
+      li.appendChild(a);
+      dotsContainer.appendChild(li);
     });
+  }
 
-    Array.from(document.querySelectorAll("a.dot")).forEach((dot, i) =>
-      dot.classList.toggle("activeDot", i === _config.activeIndex)
-    );
+  function _updateDots() {
+    document
+      .querySelector("ul.dots > li > a.activeDot")
+      ?.classList.remove("activeDot");
+
+    document
+      .querySelectorAll("ul.dots > li > a")
+      [_config.activeIndex].classList.add("activeDot");
   }
 
   function _addEventListeners() {
-    _moveTo();
+    document.addEventListener("DOMContentLoaded", () =>
+      _moveTo(_config.activeIndex)
+    );
 
     document.querySelector(".right-arrow").addEventListener("click", () => {
-      ++_config.activeIndex;
-      if (_config.activeIndex === _config.numSlides) _config.activeIndex = 0;
-      _moveTo();
+      _moveTo(++_config.activeIndex);
       _clearAutoPlay();
+      _updateDots();
     });
 
     document.querySelector(".left-arrow").addEventListener("click", () => {
-      --_config.activeIndex;
-      if (_config.activeIndex < 0) _config.activeIndex = _config.numSlides - 1;
-
-      _moveTo();
+      _moveTo(--_config.activeIndex);
       _clearAutoPlay();
+      _updateDots();
     });
 
-    Array.from(document.querySelectorAll("a.dot")).forEach((dot, i) => {
+    Array.from(document.querySelector("ul.dots").children).forEach((dot, i) => {
       dot.addEventListener("click", () => {
-        _config.activeIndex = i;
-        _moveTo();
+        _moveTo(_config.activeIndex);
         _clearAutoPlay();
+        _updateDots();
       });
     });
 
     document.addEventListener("keydown", (e) => {
       if (e.key === "ArrowRight") {
-        ++_config.activeIndex;
-        if (_config.activeIndex === _config.numSlides) _config.activeIndex = 0;
-        _moveTo();
+        _moveTo(++_config.activeIndex);
         _clearAutoPlay();
       } else if (e.key === "ArrowLeft") {
-        --_config.activeIndex;
-        if (_config.activeIndex < 0)
-          _config.activeIndex = _config.numSlides - 1;
-        _moveTo();
+        _moveTo(--_config.activeIndex);
         _clearAutoPlay();
       }
     });
   }
 
+  function _moveTo(index) {
+    const numSlides = _config.slides.length;
+    if (index === numSlides) {
+      _config.activeIndex = 0;
+    } else if (index === -1) {
+      _config.activeIndex = numSlides - 1;
+    } else {
+      _config.activeIndex = index;
+    }
+
+    _config.slides.forEach((slide, i) => {
+      slide.classList.toggle("active", i === _config.activeIndex);
+      if (_config.activeIndex === numSlides - 1) {
+        slide.classList.toggle("next", i === 0);
+      } else slide.classList.toggle("next", i === _config.activeIndex + 1);
+
+      if (_config.activeIndex === 0) {
+        slide.classList.toggle("prev", i === numSlides - 1);
+      } else slide.classList.toggle("prev", i === _config.activeIndex - 1);
+    });
+  }
+
   function _autoPlay() {
     intervalId = setInterval(() => {
-      ++_config.activeIndex;
-      if (_config.activeIndex === _config.numSlides) _config.activeIndex = 0;
-      _moveTo();
+      _moveTo(++_config.activeIndex);
+      _updateDots();
     }, _config.autoSlideInterval);
   }
 
@@ -131,10 +126,7 @@ let Slider = function ({
     );
   }
 
-  _init();
-  _addEventListeners();
-  _dynamicDimensions();
-  _autoPlay();
+  init();
 };
 
 window.Slider = Slider;
